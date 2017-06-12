1d9b26bd595645b1f6b62feaf4fed7a2d117c057
Erich Kauffman <kauffm2@stolaf.edu>
2017-06-12 06:47:56 -0500

    Small changes

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
-import express from 'express'
-import db from 'sqlite'
-import bodyParser from 'body-parser'
-import Promise from 'bluebird'
+import express from 'express';
+import db from 'sqlite';
+import bodyParser from 'body-parser';
+import Promise from 'bluebird';
 const app = express();
 
 app.use(bodyParser.json());
 
-let allBooks = async () => {
-  let books = await db.all('SELECT *, rowid FROM books ORDER BY authors, title');
-  return books;
-}
 
-app.get('/all', async (req, res) => {
-  let books = await allBooks();
+app.get('/', async (req, res) => {
+  let books = await db.all('SELECT *, rowid FROM books ORDER BY authors, title');
   res.send(books);
 });
 
-app.post('/all', async (req, res) => {
+app.post('/item', async (req, res) => {
   try{
-    let title = req.body.title;
-    let authors = req.body.authors;
-    let isbn = req.body.isbn;
-    let edition = req.body.edition;
-    let printing = req.body.printing;
-    let yr_print = req.body.yr_print;
-    let date_purch = req.body.date_purch;
-    let condition = req.body.condition;
-    let cover = req.body.cover;
-    let loc_purch = req.body.loc_purch;
-    let amt_paid = req.body.amt_paid;
-    let sell_price = req.body.sell_price;
-    let site = req.body.site;
-    db.run('INSERT INTO books VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)', title, authors, isbn, edition, printing, yr_print, date_purch, condition, cover, loc_purch, amt_paid, sell_price, site);
-    res.send();
+    db.run('INSERT INTO books VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)',
+           req.body.title,
+           req.body.authors,
+           req.body.isbn,
+           req.body.edition,
+           req.body.printing,
+           req.body.yr_print,
+           req.body.date_purch,
+           req.body.condition,
+           req.body.cover,
+           req.body.loc_purch,
+           req.body.amt_paid,
+           req.body.sell_price,
+           req.body.site
+          );
+    res.sendStatus(201);
   }catch(err){
     console.error(err);
+    res.sendStatus(500);
   }
 });
 
-app.delete('/all', async (req, res) => {
-  db.run('DELETE FROM books WHERE rowid = ?', req.body.id);
-  res.send();
+app.delete('/item/:itemNumber', async (req, res) => {
+  try{
+    db.run('DELETE FROM books WHERE rowid = ?', req.params.itemNumber);
+    res.sendStatus(200);
+  }catch(err){
+    console.err(err);
+    res.sendStatus(500);
+  }
 });
 
 process.on('exit', () => {
@@ -50,4 +53,4 @@ process.on('exit', () => {
 
 Promise.resolve().then(() => db.open('../Inventory', {Promise}))
                  .catch(err => console.error(err.stack))
-                 .finally(() => app.listen(5000, function(){console.log("Running...")}));
+                 .finally(() => app.listen(8000, function(){console.log("Running...")}));
